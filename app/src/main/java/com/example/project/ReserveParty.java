b9be2a99cc5fdd8a0589f785aa6c793a2932ac6d
Southern Winn <southerncwinn@gmail.com>
2019-04-17 21:23:22 -0500

    commit5

--- app/src/main/java/com/example/project/ReserveParty.java
+++ app/src/main/java/com/example/project/ReserveParty.java
@@ -5,7 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.widget.TimePicker;
 
 public class ReserveParty extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class ReserveParty extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserve_party);
 
+        new TimePicker(new ContextThemeWrapper(ReserveParty.this, android.R.style.Widget_Holo_Light));
     }
 
 }
