3bbacf8e5aba72b9df8369135e361585d8c651e4
Southern Winn <southerncwinn@gmail.com>
2019-05-01 12:22:09 -0500

    commit 4/30

--- app/src/main/java/com/example/project/ReserveParty.java
+++ app/src/main/java/com/example/project/ReserveParty.java
@@ -2,6 +2,7 @@ package com.example.project;
 
 import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,11 +11,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import com.google.firebase.FirebaseError;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseExceptionMapper;
+import com.google.firebase.FirebaseNetworkException;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -27,10 +34,13 @@ public class ReserveParty extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     DatePickerDialog datePickerDialog;
-    TextView txtDate;
-    Button btnDatePicker, btnReserve;
+    TextView txtDate, txtTime, txtPartyColors, txtPartyLocation, txtPartyName,
+    txtNumGuests, txtTheme;
+    Switch switchBalloons, switchCake;
+    Button btnReserve;
     Calendar calendar;
     int year, month, dayOfMonth;
+    String balloons, cake = "No";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,16 +48,41 @@ public class ReserveParty extends AppCompatActivity {
         setContentView(R.layout.activity_reserve_party);
 
 
-        new TimePicker(new ContextThemeWrapper(ReserveParty.this, android.R.style.Widget_Holo_Light));
         firebaseAuth = FirebaseAuth.getInstance();
         btnReserve = findViewById(R.id.btnReserve);
-        btnDatePicker = findViewById(R.id.btnSelectDate);
         txtDate = findViewById(R.id.txtDate);
+        txtTime = findViewById(R.id.txtPartyTime);
+        txtPartyColors = findViewById(R.id.txtPartyColors);
+        txtPartyLocation = findViewById(R.id.txtLocation);
+        txtPartyName = findViewById(R.id.txtPartyName);
+        txtNumGuests = findViewById(R.id.txtNumGuests);
+        txtTheme = findViewById(R.id.txtPartyTheme);
+       switchBalloons = findViewById(R.id.switchBalloons);
+       switchCake = findViewById(R.id.switchCake);
 
-        Switch cakeSwitch = findViewById(R.id.switchBalloons);
+
+        txtTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar currentTime = Calendar.getInstance();
+                int hour = currentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = currentTime.get(Calendar.MINUTE);
 
 
-        btnDatePicker.setOnClickListener(new View.OnClickListener() {
+
+                TimePickerDialog timePickerDialog;
+                timePickerDialog = new TimePickerDialog(ReserveParty.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        txtTime.setText(hourOfDay + ":" + minute + " PM");  // we'll just assume PM
+                    }
+                }, hour, minute, true);
+                timePickerDialog.setTitle("select party time");
+                timePickerDialog.show();
+            }
+        });
+
+        txtDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 calendar = Calendar.getInstance();
@@ -65,18 +100,41 @@ public class ReserveParty extends AppCompatActivity {
         });
 
 
+    switchBalloons.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (isChecked)
+                balloons ="Yes";
+        }
+    });
 
-                        // cakeSwitch.setOnCheckedChangeListener
+                        switchCake.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                if (isChecked)
+                                    cake ="Yes";
+                            }
+                        });
 
                         btnReserve.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-
+                        if (txtDate.getText().toString().isEmpty() || txtTime.getText().toString().isEmpty() || txtNumGuests.getText().toString().isEmpty() ||
+                        txtPartyColors.getText().toString().isEmpty() || txtPartyLocation.getText().toString().isEmpty() || txtPartyName.getText().toString().isEmpty() || txtTheme.getText().toString().isEmpty()){
+                            Toast.makeText(ReserveParty.this, "Please fill out the required information", Toast.LENGTH_LONG).show();
+                            return;
+                        }else {
+                            try {
                                 FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
                                 DatabaseReference ref = firebaseDatabase.getReference(firebaseAuth.getUid());
-                                Party newParty = new Party("Yes", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes");
+                                Party newParty = new Party(balloons, cake, txtPartyColors.getText().toString(), txtDate.getText().toString(), txtPartyLocation.getText().toString(),
+                                        txtPartyName.getText().toString(), txtNumGuests.getText().toString(), txtTheme.getText().toString(), txtTime.getText().toString());
                                 ref.child("Party").setValue(newParty);
-
+                                Toast.makeText(ReserveParty.this,"Party successfully reserved. We will be in contact soon!", Toast.LENGTH_LONG).show();
+                            }catch(Exception e){
+                                Toast.makeText(ReserveParty.this,"Error: " + e, Toast.LENGTH_LONG).show();
+                            }
+                        }
                             }
                         });
     }
